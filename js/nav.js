50da5517e4f0d46c0f1f1826f67c5555fee502d7
Zsuzsa <73961846+zhajdu99@users.noreply.github.com>
2022-05-29 12:09:42 +0200

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,41 @@
+const beforetag = "<div class='row'><button class='todouncheck '><span class='material-symbols-outlined'>radio_button_unchecked</span></button><button class='todocheck hidden'><span class='material-symbols-outlined'>radio_button_checked</span></button><p>";
+const aftertag = "</p><button class='deltoggle'><span class='material-symbols-outlined font-red'>delete</span></button></div>";
+
+$(document).ready(function() {
+    $("#navopentoggle").click(function() {
+        $("#navbar").slideToggle("slow");
+    });
+
+    $("#navbar").click(function() {
+        $("#navbar").hide("slow");
+    });
+
+    $(".notepad").on("click", ".deltoggle", function(){
+    	$(this).parent().remove();
+  	});
+
+    $("#newtaskbtn").click(function(){
+        $(".newtask").before(beforetag+$("input").val()+aftertag);
+        $("input").val("");
+    });
+
+    $(".todocheck").click(function() {
+        $("#navbar").hide("slow");
+    });
+
+    $(".todouncheck").click(function() {
+        $("#navbar").hide("slow");
+    });
+
+    $(".notepad").on("click", ".todouncheck", function(){
+    	$(this).siblings("p").addClass("cross");
+        $(this).hide();
+        $(this).siblings(".todocheck").show();
+  	});
+
+    $(".notepad").on("click", ".todocheck", function(){
+    	$(this).siblings("p").removeClass("cross");
+        $(this).hide();
+        $(this).siblings(".todouncheck").show();
+  	});
+});
